c124317c23062665d782a318feacb85bbc990d91
Omsri123 <omsridabberu@gmail.com>
2025-10-27 21:35:12 +0530

    BAsic add func

--- todo/todo_main/views.py
+++ todo/todo_main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from todo.models import Task
 
 def home(request):
-    return render(request, 'index.html')
+    tasks = Task.objects.filter(is_completed = False).order_by('-updates_at')
+    context = {
+        'tasks' : tasks,
+    }
+    return render(request, 'index.html', context)
