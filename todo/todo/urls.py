f5dc9c027c79fe6df5e0b7a3ca3e7d1648d68c20
Omsri123 <omsridabberu@gmail.com>
2025-10-29 00:58:43 +0530

    todo done

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask,name='addTask')
+    path('addTask/', views.addTask,name='addTask'),
+    path('marksAsDone/<int:pk>/', views.markAsDone,name='markAsDone'),
+    path('marksAsUndone/<int:pk>/', views.markAsUndone,name='markAsUndone'),
+    path('editTask/<int:pk>/',views.editTask,name='editTask'),
+    path('deleteTask/<int:pk>/',views.deleteTask,name='deleteTask'),
 ]
--- todo/todo_main/views.py
+++ todo/todo_main/views.py
@@ -3,7 +3,10 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updates_at')
+    cp_tasks = Task.objects.filter(is_completed = True)
+    print(cp_tasks)
     context = {
         'tasks' : tasks,
+        'cpTasks' : cp_tasks,
     }
     return render(request, 'index.html', context)
