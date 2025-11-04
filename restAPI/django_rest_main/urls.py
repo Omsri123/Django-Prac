3e3d3b0f4397ae4a8bfa146127aea9810cd4d8af
Omsri123 <omsridabberu@gmail.com>
2025-11-04 16:08:10 +0530

    till searializers

--- restAPI/django_rest_main/urls.py
+++ restAPI/django_rest_main/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('students/', include('students.urls')),
+
+    path('api/v1/',include('api.urls'))
 ]
