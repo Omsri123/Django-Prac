0b274adfad8eb9c74737c5224d036d2a1b0295d4
Omsri123 <omsridabberu@gmail.com>
2025-11-07 13:07:43 +0530

    7/11

--- restAPI/api/urls.py
+++ restAPI/api/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('students/',views.studentsView),
     path('students/<int:pk>/',views.studentDetailView),
+
     path('employees/',views.Employees.as_view()),
+    path('employees/<int:pk>/',views.EmployeeData.as_view()),
+
 ]
